e6651711f7deca508ffc05282ade2299d1010438
snolly4462gv <snolly.gv@gmail.com>
2018-06-22 17:32:29 +0300

    settings photo

--- src/app/systemModule/settings/personal-info/personal-info.component.ts
+++ src/app/systemModule/settings/personal-info/personal-info.component.ts
@@ -89,6 +89,13 @@ export class PersonalInfoComponent extends BaseComponent implements OnInit, OnCh
       .subscribe(
           (res)=>{
               this.User = res;
+              if(this.User.image_id){
+                this.main.imagesService.GetImageById(this.User.image_id)
+                  .subscribe((res)=>{
+                    console.log(`res img`, res);
+                    this.User.image_base64 = res.base64;
+                  })
+              }
               this.errorCmp.OpenWindow(BaseMessages.Success);
               console.log(`res`,this.User);
               
@@ -168,7 +175,16 @@ export class PersonalInfoComponent extends BaseComponent implements OnInit, OnCh
           }
       );
     }
+  }
 
+  uploadImage($event){
+    this.ReadImages(
+        $event.target.files,
+        (res:string)=>{
+            this.User.image_base64 = res;
+            // this.isNewImage = true;
+        }
+    );
   }
   
 
