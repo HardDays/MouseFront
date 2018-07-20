a44bbc10c8cb829aa322f2d43a587bd199f02636
unknown <snolly.gv@gmail.com>
2018-07-20 16:55:34 +0300

    fix admin open artist event

--- src/app/core/services/admin.service.ts
+++ src/app/core/services/admin.service.ts
@@ -17,16 +17,36 @@ export class AdminService{
     constructor(private http: HttpService, private typeService:TypeService){
     }
 
+    Statuses = {
+        "just_added": "new",
+        "pending": "pending",
+        "approved": "approved",
+        "denied": "denied",
+        "active": "active",
+        "inactive": "inactive"
+    }
+
     CreateAdmin(user:UserCreateModel){
         return this.http.CommonRequest(
-            ()=> this.http.PostData('/admin/create_admin.json',JSON.stringify(user))
+            ()=> this.http.PostData('/admin.json',JSON.stringify(user))
         );
     }
+
+    PatchAdmin(user:UserCreateModel,id:number){
+        return this.http.CommonRequest(
+            ()=> this.http.PostData('/admin/'+id+'.json',JSON.stringify(user))
+        );
+    }
+
+
+    //////////////////////////////////////
+    //////         ACCOUNTS         //////   
+    //////////////////////////////////////
 
     GetNewAccountsCount()
     {
         return this.http.CommonRequest(
-            ()=> this.http.GetData('/admin/new_accounts_count.json','')
+            ()=> this.http.GetData('/admin/accounts/new_count.json','')
         );
     }
 
@@ -43,6 +63,62 @@ export class AdminService{
         );
     }
 
+    GetAccountsNew()
+    {
+        return this.http.CommonRequest(
+            ()=> this.http.GetData('/admin/accounts/new.json', '')
+        );
+    }
+
+    GetAccountsUserUsage()
+    {
+        return this.http.CommonRequest(
+            ()=> this.http.GetData('/admin/accounts/user_usage.json', '')
+        );
+    }
+
+    GetAccountById(id:number)
+    {
+        return this.http.CommonRequest(
+            ()=> this.http.GetData('/admin/accounts/'+id+'.json', '')
+        );
+    }
+
+    AccountApprove(id:number)
+    {
+        return this.http.CommonRequest(
+            ()=> this.http.PostData('/admin/accounts/'+id+'/approve.json', JSON.stringify({id}))
+        );
+    }
+
+    AccountDeny(id:number)
+    {
+        return this.http.CommonRequest(
+            ()=> this.http.PostData('/admin/accounts/'+id+'/deny.json', JSON.stringify({id}))
+        );
+    }
+
+    AccountDelete(id:number)
+    {
+        return this.http.CommonRequest(
+            ()=> this.http.DeleteDataWithBody('/admin/accounts/'+id+'.json', JSON.stringify({id}))
+        );
+    }
+
+
+
+    //////////////////////////////////////
+    //////         EVENTS           //////   
+    //////////////////////////////////////
+
+
+    GetNewEventsCount(params?)
+    {
+        return this.http.CommonRequest(
+            ()=> this.http.GetData('/admin/events/new_count.json', '')
+        );
+    }
+
     GetEventsRequests(params?)
     {
         return this.http.CommonRequest(
@@ -50,6 +126,33 @@ export class AdminService{
         );
     }
 
+    GetEventById(id)
+    {
+        return this.http.CommonRequest(
+            ()=> this.http.GetData('/admin/events/'+id+'.json', this.typeService.ParamsToUrlSearchParams({id}))
+        );
+    }
+
+    EventApprove(id:number)
+    {
+        return this.http.CommonRequest(
+            ()=> this.http.PostData('/admin/events/'+id+'/approve.json', JSON.stringify({id}))
+        );
+    }
+
+    EventDeny(id:number)
+    {
+        return this.http.CommonRequest(
+            ()=> this.http.PostData('/admin/events/'+id+'/deny.json', JSON.stringify({id}))
+        );
+    }
+
+    EventDelete(id:number)
+    {
+        return this.http.CommonRequest(
+            ()=> this.http.DeleteDataWithBody('/admin/events/'+id+'.json', JSON.stringify({id}))
+        );
+    }
 
 
 
