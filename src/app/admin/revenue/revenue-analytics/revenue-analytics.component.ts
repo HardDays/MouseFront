d39e428f412f47dbc9fd071f3c6a61d9505ac1e2
unknown <snolly.gv@gmail.com>
2018-07-23 14:59:20 +0300

    admin all pages

--- src/app/admin/revenue/revenue-analytics/revenue-analytics.component.ts
+++ src/app/admin/revenue/revenue-analytics/revenue-analytics.component.ts
@@ -1,21 +1,182 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '../../../core/base/base.component';
 
 @Component({
   selector: 'app-revenue-analytics',
   templateUrl: './revenue-analytics.component.html',
   styleUrls: ['./revenue-analytics.component.css']
 })
-export class RevenueAnalyticsComponent implements OnInit {
+export class RevenueAnalyticsComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  citiesTotal= {total:0 , cities:[{price:0,address:''}]};
+  citiesSales = {total:0 , cities:[{price:0,address:''}]};
+
+
+  countsAdvertisingTotal = 0;
+  countsAdvertisingSales = 0;
+
+
+  countsArtistTotal = 0;
+  countsArtistSales = 0;
+
+  countsDateTotal = 0;
+  countsDateSales = 0;
+
+  countsFundingTotal = 0;
+  countsFundingSales = 0;
+
+  countsTicketsTotal = 0;
+  countsTicketsSales = 0;
+
+  countsVenueTotal = 0;
+  countsVenueSales = 0;
+
+  countsVrTotal = 0;
+  countsVrSales = 0;
+
+
+
 
   ngOnInit() {
+    console.log();
+    this.main.adminService.GetRevenueCities('total')
+      .subscribe(
+        (res)=>{
+          this.citiesTotal = res;
+        }
+      )
+
+    this.main.adminService.GetRevenueCities('sales')
+      .subscribe(
+        (res)=>{
+          this.citiesSales = res;
+        }
+      )
+
+
+
+      this.main.adminService.GetRevenueCountsAdvertising('total','all')
+      .subscribe(
+        (res)=>{
+          this.countsAdvertisingTotal = res;
+        }
+      )
+      this.main.adminService.GetRevenueCountsAdvertising('sales','all')
+      .subscribe(
+        (res)=>{
+          this.countsAdvertisingSales = res;
+        }
+      )
+
+
+
+      this.main.adminService.GetRevenueCountsArtist('total','all')
+      .subscribe(
+        (res)=>{
+          this.countsArtistTotal = res;
+        }
+      )
+      this.main.adminService.GetRevenueCountsArtist('sales','all')
+      .subscribe(
+        (res)=>{
+          this.countsArtistSales = res;
+        }
+      )
+
+
+
+      this.main.adminService.GetRevenueCountsDate('total','all')
+      .subscribe(
+        (res)=>{
+          this.countsDateTotal = res;
+        }
+      )
+      this.main.adminService.GetRevenueCountsDate('sales','all')
+      .subscribe(
+        (res)=>{
+          this.countsDateSales = res;
+        }
+      )
+
+
+
+      this.main.adminService.GetRevenueCountsFunding('total','all','regular')
+      .subscribe(
+        (res)=>{
+          this.countsFundingTotal = res;
+        }
+      )
+      this.main.adminService.GetRevenueCountsFunding('sales','all','regular')
+      .subscribe(
+        (res)=>{
+          this.countsFundingSales = res;
+        }
+      )
+
+
+
+      this.main.adminService.GetRevenueCountsTickets('total','all')
+      .subscribe(
+        (res)=>{
+          this.countsTicketsTotal = res;
+        }
+      )
+      this.main.adminService.GetRevenueCountsTickets('sales','all')
+      .subscribe(
+        (res)=>{
+          this.countsTicketsSales = res;
+        }
+      )
+
+
+
+      this.main.adminService.GetRevenueCountsVenue('total','all')
+      .subscribe(
+        (res)=>{
+          this.countsVenueTotal = res;
+        }
+      )
+      this.main.adminService.GetRevenueCountsVenue('sales','all')
+      .subscribe(
+        (res)=>{
+          this.countsVenueSales = res;
+        }
+      )
+
+
+
+
+      this.main.adminService.GetRevenueCountsVr('total','all')
+      .subscribe(
+        (res)=>{
+          this.countsVrTotal = res;
+        }
+      )
+      this.main.adminService.GetRevenueCountsVr('sales','all')
+      .subscribe(
+        (res)=>{
+          this.countsVrSales = res;
+        }
+      )
+
+
+
+
+
+
+
+
   }
 
 
 
-
-
+indexColor = 0;
+nextColor(){
+  let colors = ['#991f72','#d2365d','#5157b8','#f2ad0f','#942541','#079391'];
+  let color = colors[this.indexColor%colors.length];
+  this.indexColor = this.indexColor+1;
+  return {'background-color':color};
+}
 
 
   public lineChartData:Array<any> = [
