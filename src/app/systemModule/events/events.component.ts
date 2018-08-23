8dd5786b27589d19b5b6cdf6e7d596228999b65b
unknown <snolly.gv@gmail.com>
2018-08-23 11:48:51 +0300

    fix geo

--- src/app/systemModule/events/events.component.ts
+++ src/app/systemModule/events/events.component.ts
@@ -94,11 +94,44 @@ export class EventsComponent extends BaseComponent implements OnInit,AfterViewCh
         this.openSearch();
         this.setHeightSearch();
 
-        navigator.geolocation.getCurrentPosition((position) => {
-            this.MyCoords.lat = position.coords.latitude;
-            this.MyCoords.lng = position.coords.longitude - 2;
-        })
+        if(navigator.geolocation && !(navigator.geolocation === undefined)){
+            console.log(`navigator.geolocation`);
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.MyCoords.lat = position.coords.latitude;
+                this.MyCoords.lng = position.coords.longitude - 2;
+
+                console.log(`position.coords`,position.coords,this.MyCoords);
+                if(this.MyCoords.lat === 0 && this.MyCoords.lng === 0)
+                    this.getPosition();
+
+            },(err)=>{
+                console.log(`error`);
+                this.getPosition();
+            },{
+                timeout: 10000
+            });
+        } else {
+           this.getPosition()
+        }
     }
+
+    getPosition(){
+        //console.log(`api.ipstack`);
+        // $.getJSON('http://api.ipstack.com/check?access_key=428075a8fe82f2d6de7696b9bfec35b8', (data)=>{
+        //     console.log(data);
+        //     this.MyCoords.lat = data.latitude;
+        //     this.MyCoords.lng = data.longitude - 2;
+        // });
+        console.log(`service`);
+        this.main.accService.GetLocation()
+            .subscribe((data)=>{
+                console.log(`data`,data);
+                this.MyCoords.lat = data.location[0];
+                this.MyCoords.lng = data.location[1] - 2;
+            })
+
+    }
+
     @ViewChild('search') search: SearchEventsComponent;
     
     @ViewChild('mapForm') mapForm : SearchEventsMapComponent;
