b1332090ec85f38da51c866f55ab2a9db854dee0
prmnr2k <prmn.r2k@gmail.com>
2018-09-09 19:21:52 +0300

    microfix of show detail content

--- src/app/systemModule/showsDetail/showsDetail.component.ts
+++ src/app/systemModule/showsDetail/showsDetail.component.ts
@@ -157,7 +157,7 @@ export class ShowsDetailComponent extends BaseComponent implements OnInit,AfterV
     InitEvent(event:EventGetModel)
     {
         this.Event = event;
-
+        // console.log(this.Event);
         if(this.Event.hashtag)
             this.Event.hashtag = this.Event.hashtag.replace("#","");
 
@@ -244,10 +244,18 @@ export class ShowsDetailComponent extends BaseComponent implements OnInit,AfterV
     {
         this.TicketsToBuy.push(object);
         this.CalculateCurrentPrice();
-        this.OpenErrorWindow(object.count + this.GetTranslateString(" ticket") 
-        + (object.count > 1 && object.count <= 4 ?this.GetTranslateString("addRuA"): "") 
-        + (object.count > 4 ?this.GetTranslateString("addRuOv"): " ") 
-        + (object.count > 1 ?this.GetTranslateString("addedRu"): this.GetTranslateString("added")) 
+        this.OpenErrorWindow(object.count + this.GetTranslateString(" ticket")
+        + (object.count > 1  ? 
+            (object.count < 5 ?
+                this.GetTranslateString("addRuA")
+                : (object.count > 4 ?
+                    this.GetTranslateString("addRuOv")
+                    : ""
+                )
+            ) + this.GetTranslateString("addedRu")
+            : " " + this.GetTranslateString("added")
+        )
+        + " "
         + this.GetTranslateString("to your cart!"));
     }
 
