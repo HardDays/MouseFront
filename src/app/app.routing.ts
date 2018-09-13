2c612b93b1875f540d320fda29524d0a6ce38da2
unknown <snolly.gv@gmail.com>
2018-09-14 02:50:45 +0300

    pull

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -10,14 +10,15 @@ import { SocialNewAccComponent } from './access/login/social-new-acc/social-new-
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'access', pathMatch:'full'},
-    { path: 'access',component: StupidAccessComponent, canActivate: [AppAccessGuard]},
+    // { path: '', redirectTo: 'access', pathMatch:'full'},
+    { path: '', redirectTo: 'system/shows', pathMatch:'full'},
+    // { path: 'access',component: StupidAccessComponent, canActivate: [AppAccessGuard]},
     { path: 'login',component: LoginComponent, canActivate: [AppAccessGuard]},
     { path: 'social',component: SocialNewAccComponent, canActivate: [AppAccessGuard]},
     { path: 'register',component: RegistrationComponent, canActivate: [AppAccessGuard]},
     { path: 'system', loadChildren: './systemModule/system.module#SystemModule'},
     { path: 'admin', loadChildren: './admin/admin.module#AdminModule'},
-    { path: '**', redirectTo: 'access'}
+    { path: '**', redirectTo: 'system/shows'}
 ];
 
 @NgModule({
