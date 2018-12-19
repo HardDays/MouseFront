8ddab2ee84fa76d8ecd96206bfb9ae9df9ea0adb
unknown <snolly.gv@gmail.com>
2018-12-19 21:58:07 +0300

    add fullscrean

--- src/app/systemModule/vr/vr.component.ts
+++ src/app/systemModule/vr/vr.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { windowTime } from 'rxjs/operators';
 
+declare var OmniVirt;
 @Component({
   selector: 'app-vr',
   templateUrl: './vr.component.html',
@@ -13,11 +15,35 @@ export class VrComponent implements OnInit {
 
   constructor(
     protected _sanitizer     : DomSanitizer
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.VRVideoLink = this._sanitizer.bypassSecurityTrustResourceUrl(
       'http://www.vroptimal-3dx-assets.com/content/' + this.ContentId + '?player=true&autoplay=true&referer=' + encodeURIComponent(window.location.href));
+  }
+
+  ngAfterViewInit() {
+    var _the = this;
+
+      OmniVirt.api.recieveMessage(window, 'load', function(type, data, iframe) {
+        setTimeout(() => {
+          if(_the.ContentId && _the.VRVideoLink)
+            _the.fullscreen();
+        }, 100);
+      });
+
+      OmniVirt.api.recieveMessage(window, 'collapsed', function(type, data, iframe) {
+        console.log(`Closed`);
+      });
+
+  }
+
+  fullscreen() {
+    OmniVirt.api.sendMessage(
+                'expand',
+                null,
+                document.getElementById('ado-'+this.ContentId)
+        );
   }
 
 }
